341183d0331f5bdfcaaffa4adbbc33dde74fa232
PedroFernandesG <147552248+PedroFernandesG@users.noreply.github.com>
2024-09-07 23:17:21 -0300

    Update NumProvinces.cpp

--- Teoria dos Grafos/LeetCode/NumProvinces.cpp
+++ Teoria dos Grafos/LeetCode/NumProvinces.cpp
@@ -1,1 +1,38 @@
+/*
+Number Of Provinces - LeetCode
+Question 547
+*/
 
+class Solution {
+public:
+
+    void DFS(int v, bool visited[], vector<vector<int>>& isConnected,int n){
+
+        visited[v] = true;
+
+        for(int i = 0;i < n;i++){
+            if(isConnected[v][i] == 1 && !visited[i]){
+                DFS(i, visited, isConnected,n);
+            }
+        }
+    }
+
+    int findCircleNum(vector<vector<int>>& isConnected){
+        int n = isConnected.size();
+        bool visited[n];
+
+        for(int i = 0;i < n;i++)
+            visited[i] = false;
+
+        int comp = 0;
+
+        for(int i = 0;i < n;i++){
+            if(!visited[i]){
+                comp++;
+
+                DFS(i, visited, isConnected,n);
+            }
+        }
+        return comp;
+    }
+};
